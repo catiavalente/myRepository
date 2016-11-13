9a4350ef29451b0dc65d1aa39a6c75ac3f857aa4
catiavalente <catiavalente@ua.pt>
2016-11-13 10:54:15 +0000

    Node counter v1.0

---
+++ node_counter/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var count = 0; //contador
+var port = 4000; //porto de escuta
+server.listen(port);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('count', count);
+  socket.on('inc', function () {
+    count++;
+    io.sockets.emit('count', count);
+  });
+});
